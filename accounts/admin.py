8558bcb173ca1465df6b36eff3982c32d1b7b9b6
LudgeAnies <92254157+LudgeAnies@users.noreply.github.com>
2022-10-22 01:51:09 +0300

    add session user

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,20 +3,21 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser, Level
+from .models import CustomUser, Level, Session
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'level', 'money']
+    list_display = ['email', 'username', 'level', 'money', 'session_user']
     fieldsets = (
         (None, {
-            'fields': ('email', 'username', 'password', 'level', 'money')
+            'fields': ('email', 'username', 'password', 'level', 'money', 'session_user')
         }),
     )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Level)
+admin.site.register(Session)
--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Session(models.Model):
+    session_one = models.BooleanField()
+    session_two = models.BooleanField()
+    session_three = models.BooleanField()
+    session_four = models.BooleanField()
+    session_five = models.BooleanField()
+    session_six = models.BooleanField()
+    session_seven = models.BooleanField()
+    session_eight = models.BooleanField()
+    session_nine = models.BooleanField()
+    session_ten = models.BooleanField()
+
+
 class Level(models.Model):
     easy = models.BooleanField(verbose_name='Легкий')
     normal = models.BooleanField(verbose_name='Нормальный')
@@ -11,6 +24,7 @@ class Level(models.Model):
 class CustomUser(AbstractUser):
     level = models.ForeignKey(Level, on_delete=models.CASCADE, null=True, blank=True)
     money = models.IntegerField(default=40000)
+    session_user = models.ForeignKey(Session, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.username
